edfbc713468c14463921839ee88f83e534d6f43b
frankopkusianwar <okirorfrank3@gmail.com>
2020-05-19 13:28:08 +0300

    complete post model

--- app/controllers/concerns/posts_controller.rb
+++ app/controllers/concerns/posts_controller.rb
@@ -1,13 +1,20 @@
 class PostsController < ApplicationController
     def index
+        @posts = Post.all
     end
 
     def new
+        @post = Post.new
     end
-    
+
     def create
+        post = Post.new(params.require(:post).permit(:title, :content))
+        post.user_id = current_user.id
+        post.save
+        redirect_to post_path(post.id)
     end
 
     def show
+        @post = Post.find(params[:id])
     end
 end
